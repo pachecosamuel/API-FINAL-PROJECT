507e5e8df76bafc0f7641345759a68fe9683fda1
mattpsvreis <mateuspsvreis@gmail.com>
2022-05-31 20:19:24 -0300

    Serviços Email, Imagem | Adição de dependências e parâmetros

---
+++ ecommerce/src/main/java/com/residencia/ecommerce/service/EmailService.java
@@ -0,0 +1,81 @@
+package com.residencia.ecommerce.service;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import com.residencia.ecommerce.entity.Pedido;
+
+@Service
+public class EmailService {
+	@Autowired
+	JavaMailSender mailSender;
+
+	@Autowired
+	TemplateEngine templateEngine;
+
+	@Value("${spring.mail.host}")
+	private String mailHost;
+
+	@Value("${spring.mail.port}")
+	private String mailPort;
+
+	@Value("${spring.mail.username}")
+	private String mailUserName;
+
+	@Value("${spring.mail.password}")
+	private String mailPassword;
+
+	@Value("${spring.mail.default}")
+	private String mailDefault;
+
+	public EmailService(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	public void sendEmailText(String recipient, String subject, String message) {
+		SimpleMailMessage smm = new SimpleMailMessage();
+
+		smm.setTo(recipient);
+		smm.setSubject(subject);
+		smm.setText(message);
+
+		smm.setFrom(mailDefault);
+
+		mailSender.send(smm);
+	}
+
+	public void sendEmailHTML(String recipient, String subject, Pedido pedido) throws MessagingException {
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+
+		helper.setText(generateEmailHTML(pedido), true);
+		helper.setTo(recipient);
+		helper.setSubject(subject);
+		helper.setFrom(mailDefault);
+
+		mailSender.send(mimeMessage);
+	}
+
+	private String generateEmailHTML(Pedido pedido) {
+		Map<String, Object> variables = new HashMap<>();
+		variables.put("pedidotext", pedido);
+
+		final String templateFileName = "pedido";
+		String output = this.templateEngine.process(templateFileName, new Context(Locale.getDefault(), variables));
+
+		return output;
+	}
+}
