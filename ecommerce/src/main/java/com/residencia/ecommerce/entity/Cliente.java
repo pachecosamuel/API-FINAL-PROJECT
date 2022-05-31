a2b2a63baf54a1359ba1f62a0229f88c4ef19cd6
mattpsvreis <mateuspsvreis@gmail.com>
2022-05-31 20:36:05 -0300

    Removendo imports inutilizados

--- ecommerce/src/main/java/com/residencia/ecommerce/entity/Cliente.java
+++ ecommerce/src/main/java/com/residencia/ecommerce/entity/Cliente.java
@@ -1,7 +1,6 @@
 package com.residencia.ecommerce.entity;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,8 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -102,7 +99,5 @@ public class Cliente {
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
-
-	
 
 }
