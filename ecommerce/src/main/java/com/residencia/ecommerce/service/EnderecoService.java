d9b43314db8c9fb755b3d0628863dba7ee43e4c7
mattpsvreis <mateuspsvreis@gmail.com>
2022-06-01 19:49:23 -0300

    Método e DTO para recebermos CEP através da API Externa ViaCEP

--- ecommerce/src/main/java/com/residencia/ecommerce/service/EnderecoService.java
+++ ecommerce/src/main/java/com/residencia/ecommerce/service/EnderecoService.java
@@ -1,10 +1,14 @@
 package com.residencia.ecommerce.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.residencia.ecommerce.dto.CepDTO;
 import com.residencia.ecommerce.entity.Endereco;
 import com.residencia.ecommerce.repository.EnderecoRepository;
 
@@ -34,4 +38,12 @@ public class EnderecoService {
 		enderecoRepository.deleteById(id);
 	}
 
+	public CepDTO getCepDTOFromExternal(String cep) {
+		RestTemplate restTemplate = new RestTemplate();
+		String uri = "https://viacep.com.br/ws/{cep}/json/";
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("cep", cep);
+
+		return restTemplate.getForObject(uri, CepDTO.class, params);
+	}
 }
