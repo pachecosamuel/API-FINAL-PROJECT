b222bcd6f62adfc32e5be8f4024dd53726f657b8
mattpsvreis <mateuspsvreis@gmail.com>
2022-06-01 18:40:13 -0300

    Removendo comentários Email Service, alterando template HTML, Re-fatorando Exceptions

--- ecommerce/src/main/java/com/residencia/ecommerce/exception/ErrorResponse.java
+++ ecommerce/src/main/java/com/residencia/ecommerce/exception/ErrorResponse.java
@@ -1,17 +1,24 @@
 package com.residencia.ecommerce.exception;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
-
 	private final int status;
 	private final String message;
+	private List<String> details;
 
 	public ErrorResponse(int status, String message) {
-		super();
 		this.status = status;
 		this.message = message;
+	}
+
+	public ErrorResponse(int status, String message, List<String> details) {
+		this.status = status;
+		this.message = message;
+		this.details = details;
 	}
 
 	public int getStatus() {
@@ -21,5 +28,13 @@ public class ErrorResponse {
 	public String getMessage() {
 		return message;
 	}
-	
+
+	public List<String> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<String> details) {
+		this.details = details;
+	}
+
 }
